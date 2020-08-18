9a111c709fda0d3a103999600cbb94af14b787bb
martinetmayank <thedeveloper.mayank@gmail.com>
2020-08-19 01:28:05 +0530

    error handling of geolocation & weather API

--- app.js
+++ app.js
@@ -8,19 +8,28 @@ request({
     url: URL,
     json: true
 }, (error, response) => {
-    const temperature = response.body.currently.temperature
-    const rainChance = response.body.currently.precipProbability
-    const humidity = response.body.currently.humidity
-    const visibility = response.body.currently.visibility
-    const windSpeed = response.body.currently.windSpeed
-    const summary = response.body.daily.data[0].summary
 
-    console.log('Temperature ' + temperature + ' degree celsius.')
-    console.log('There is ' + rainChance + '% chance of rain today.')
-    console.log('Humidity: ' + humidity)
-    console.log('Wind Speed is ' + windSpeed + ' mps')
-    console.log('Visibility: ' + visibility)
-    console.log('Summary: ' + summary)
+    if (error) {
+        console.log('Unable to connect to weather service...')
+    } else if (response.body.error) {
+        console.log('Unable to find location...')
+    } else {
+
+        const temperature = response.body.currently.temperature
+        const rainChance = response.body.currently.precipProbability
+        const humidity = response.body.currently.humidity
+        const visibility = response.body.currently.visibility
+        const windSpeed = response.body.currently.windSpeed
+        const summary = response.body.daily.data[0].summary
+
+        console.log('Temperature ' + temperature + ' degree celsius.')
+        console.log('There is ' + rainChance + '% chance of rain today.')
+        console.log('Humidity: ' + humidity)
+        console.log('Wind Speed is ' + windSpeed + ' mps')
+        console.log('Visibility: ' + visibility)
+        console.log('Summary: ' + summary)
+
+    }
 })
 
 
@@ -31,13 +40,20 @@ Address -> Lat/Lon pair -> DarkSky API -> Result
 */
 
 const MapBoxAPI = 'pk.eyJ1IjoibWFydGluZXRtYXlhbmsiLCJhIjoiY2tlMGJqOHQ2MXJtOTMwb3NkMGFvc254biJ9.r2mocrfZoiLzOWMA_7GpSw'
-const geoCodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFydGluZXRtYXlhbmsiLCJhIjoiY2tlMGJqOHQ2MXJtOTMwb3NkMGFvc254biJ9.r2mocrfZoiLzOWMA_7GpSw&limit=1'
+const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/gurgaon.json?access_token=pk.eyJ1IjoibWFydGluZXRtYXlhbmsiLCJhIjoiY2tlMGJqOHQ2MXJtOTMwb3NkMGFvc254biJ9.r2mocrfZoiLzOWMA_7GpSw&limit=1'
 
 request({
-    url: geoCodingURL,
+    url: geoCodeURL,
     json: true
 }, (error, response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
+    if (error) {
+        console.log('Unable to connect to geoCoding Service...')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location, try another search...')
+
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 })
